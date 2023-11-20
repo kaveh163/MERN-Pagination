c8d4357967d0d25c46c5f3aae3c5fe63fd644c9b
Kaveh <kavehfrounchi@gmail.com>
2023-11-20 13:36:46 +0330

    updated Mern Pagination for page less than 5

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,34 @@ import Pagination from "./components/Pagination";
 import { useState, useEffect } from "react";
 import { Outlet, useParams } from "react-router-dom";
 const App = () => {
-  const pageNumber = useParams().pageNumber || 1;
-  console.log(pageNumber);
+  const page_Number = useParams().pageNumber || 1;
+  // console.log(pageNumber);
 
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   // if something wrong with our call to backend
   const [error, setError] = useState(false);
-  const [page, setPage] = useState(pageNumber);
+  console.log('PageNumberInState', page_Number);
+  const [page, setPage] = useState(page_Number);
   const [pages, setPages] = useState(1);
   useEffect(() => {
+    console.log('PageNumberEffect', page_Number);
     const fetchPosts = async () => {
       // we are making a request so setLoading to true
 
       setLoading(true);
       try {
-        let res = await fetch(`/api/v1/posts?page=${page}`, {
+        let res = await fetch(`/api/v1/posts?page=${page_Number}`, {
           headers: {
             accept: "application/json",
             "User-agent": "learning app",
           },
         });
         const { data, pages: totalPages } = await res.json();
+        console.log('pageNumberAfterFetch',page_Number );
         setPages(totalPages);
         setPosts(data);
+
         // after data is fetched we set loading to false
         setLoading(false);
       } catch (error) {
@@ -38,7 +42,7 @@ const App = () => {
       }
     };
     fetchPosts();
-  }, [page]);
+  }, [page_Number]);
   return (
     <div className="app">
       {loading ? (
@@ -47,13 +51,14 @@ const App = () => {
         <h3 className="error-text">{error}</h3>
       ) : (
         <>
-          <Pagination page={page} pages={pages} changePage={setPage} />
+        {console.log('pageNumberInReturnBeforePagination', page_Number)}
+          <Pagination page={page_Number} pages={pages} changePage={setPage} />
           <div className="app__posts">
             {posts.map((post) => (
               <Card key={post._id} post={post} />
             ))}
           </div>
-          <Pagination page={page} pages={pages} changePage={setPage} />
+          <Pagination page={page_Number} pages={pages} changePage={setPage} />
         </>
       )}
 
--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -1,8 +1,11 @@
 import "./Pagination.css";
-
+import { useNavigate } from "react-router-dom";
 const Pagination = ({ page, pages, changePage }) => {
+    console.log('pageNumberPropInsidePagination', page);
+  const navigate = useNavigate();
   let middlePagination;
   if (pages <= 5) {
+    console.log('firstpages', pages);
     middlePagination = [...Array(pages)].map((_, idx) => (
       <button
         key={idx + 1}
@@ -13,14 +16,19 @@ const Pagination = ({ page, pages, changePage }) => {
       </button>
     ));
   } else {
+    console.log('pages', pages);
+    console.log('pageNumberPropInsidePaginationInFirstElse', page);
     const startValue = Math.floor((page - 1) / 5) * 5;
     middlePagination = (
       <>
         {[...Array(5)].map((_, idx) => (
           <button
             key={startValue + idx + 1}
-            disabled={page === startValue + idx + 1}
-            onClick={() => changePage(startValue + idx + 1)}
+            disabled={parseInt(page) == startValue + idx + 1}
+            onClick={() => {
+              // changePage(startValue + idx + 1)
+              navigate(`/page/${startValue + idx + 1}`);
+            }}
           >
             {startValue + idx + 1}
           </button>
@@ -30,65 +38,74 @@ const Pagination = ({ page, pages, changePage }) => {
         <button onClick={() => changePage(pages)}>{pages}</button>
       </>
     );
-    if (page > 5) {
-      if (pages - page >= 5) {
-        middlePagination = (
-          <>
-            <button onClick={() => changePage(1)}>1</button>
-            <button>...</button>
-            <button onClick={() => changePage(startValue)}>{startValue}</button>
-            {[...Array(5)].map((_, idx) => (
-              <button
-                key={startValue + idx + 1}
-                disabled={page === startValue + idx + 1}
-                onClick={() => changePage(startValue + idx + 1)}
-              >
-                {startValue + idx + 1}
-              </button>
-            ))}
-            <button>...</button>
-            {/* last page */}
-            <button onClick={() => changePage(pages)}>{pages}</button>
-          </>
-        );
-      } else {
-        let amountLeft = pages - page + 5;
-        middlePagination = (
-          <>
-            <button onClick={() => changePage(1)}>1</button>
-            <button>...</button>
-            <button onClick={() => changePage(startValue)}>{startValue}</button>
-            {[...Array(amountLeft)].map((_, idx) => (
-              <button
-                key={startValue + idx + 1}
-                style={pages < startValue + idx + 1 ? {display: "none"}: null}
-                disabled={page === startValue + idx + 1}
-                onClick={() => changePage(startValue + idx + 1)}
-              >
-                {startValue + idx + 1}
-              </button>
-            ))}
-            
-          </>
-        );
-      }
-    }
+    // if (page > 5) {
+    //   if (pages - page >= 5) {
+    //     middlePagination = (
+    //       <>
+    //         <button onClick={() => changePage(1)}>1</button>
+    //         <button>...</button>
+    //         <button onClick={() => changePage(startValue)}>{startValue}</button>
+    //         {[...Array(5)].map((_, idx) => (
+    //           <button
+    //             key={startValue + idx + 1}
+    //             disabled={page === startValue + idx + 1}
+    //             onClick={() => changePage(startValue + idx + 1)}
+    //           >
+    //             {startValue + idx + 1}
+    //           </button>
+    //         ))}
+    //         <button>...</button>
+    //         {/* last page */}
+    //         <button onClick={() => changePage(pages)}>{pages}</button>
+    //       </>
+    //     );
+    //   } else {
+    //     let amountLeft = pages - page + 5;
+    //     middlePagination = (
+    //       <>
+    //         <button onClick={() => changePage(1)}>1</button>
+    //         <button>...</button>
+    //         <button onClick={() => changePage(startValue)}>{startValue}</button>
+    //         {[...Array(amountLeft)].map((_, idx) => (
+    //           <button
+    //             key={startValue + idx + 1}
+    //             style={
+    //               pages < startValue + idx + 1 ? { display: "none" } : null
+    //             }
+    //             disabled={page === startValue + idx + 1}
+    //             onClick={() => changePage(startValue + idx + 1)}
+    //           >
+    //             {startValue + idx + 1}
+    //           </button>
+    //         ))}
+    //       </>
+    //     );
+    //   }
+    // }
   }
   return (
     pages > 1 && (
       <div className="pagination">
         <button
           className="pagination__prev"
-          onClick={() => changePage((page) => page - 1)}
-          disabled={page === 1}
+          onClick={() => {
+            // changePage((page) => page - 1)
+            navigate(`/page/${parseInt(page) - 1 }`);
+          }}
+          disabled={parseInt(page) == 1}
         >
           &#171;
         </button>
         {middlePagination}
         <button
           className="pagination__next"
-          onClick={() => changePage((page) => page + 1)}
-          disabled={page === pages}
+          onClick={() => {
+            console.log('pageInsidePagination', page + 1)
+            // changePage((page) => page + 1);
+            navigate(`/page/${parseInt(page) + 1 }`);
+
+          } }
+          disabled={parseInt(page) == pages}
         >
           &#187;
         </button>
