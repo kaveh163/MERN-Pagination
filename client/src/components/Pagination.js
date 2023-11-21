3322aebfd628c81b2d9f2ad29851fb60bc6d7dbf
Kaveh <kavehfrounchi@gmail.com>
2023-11-21 12:28:20 +0330

    Finished Final MERN  Pagination

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -9,8 +9,12 @@ const Pagination = ({ page, pages, changePage }) => {
     middlePagination = [...Array(pages)].map((_, idx) => (
       <button
         key={idx + 1}
-        onClick={() => changePage(idx + 1)}
-        disabled={page === idx + 1}
+        onClick={() => {
+            // changePage(idx + 1);
+            navigate(`/page/${idx + 1}`);
+
+        }}
+        disabled={parseInt(page) === idx + 1}
       >
         {idx + 1}
       </button>
@@ -35,53 +39,86 @@ const Pagination = ({ page, pages, changePage }) => {
         ))}
         <button>...</button>
         {/* last page */}
-        <button onClick={() => changePage(pages)}>{pages}</button>
+        <button onClick={() => {
+            // changePage(pages);
+            navigate(`/page/${pages}`);
+
+        }}>{pages}</button>
       </>
     );
-    // if (page > 5) {
-    //   if (pages - page >= 5) {
-    //     middlePagination = (
-    //       <>
-    //         <button onClick={() => changePage(1)}>1</button>
-    //         <button>...</button>
-    //         <button onClick={() => changePage(startValue)}>{startValue}</button>
-    //         {[...Array(5)].map((_, idx) => (
-    //           <button
-    //             key={startValue + idx + 1}
-    //             disabled={page === startValue + idx + 1}
-    //             onClick={() => changePage(startValue + idx + 1)}
-    //           >
-    //             {startValue + idx + 1}
-    //           </button>
-    //         ))}
-    //         <button>...</button>
-    //         {/* last page */}
-    //         <button onClick={() => changePage(pages)}>{pages}</button>
-    //       </>
-    //     );
-    //   } else {
-    //     let amountLeft = pages - page + 5;
-    //     middlePagination = (
-    //       <>
-    //         <button onClick={() => changePage(1)}>1</button>
-    //         <button>...</button>
-    //         <button onClick={() => changePage(startValue)}>{startValue}</button>
-    //         {[...Array(amountLeft)].map((_, idx) => (
-    //           <button
-    //             key={startValue + idx + 1}
-    //             style={
-    //               pages < startValue + idx + 1 ? { display: "none" } : null
-    //             }
-    //             disabled={page === startValue + idx + 1}
-    //             onClick={() => changePage(startValue + idx + 1)}
-    //           >
-    //             {startValue + idx + 1}
-    //           </button>
-    //         ))}
-    //       </>
-    //     );
-    //   }
-    // }
+    if (parseInt(page) > 5) {
+      if (pages - parseInt(page) >= 5) {
+        middlePagination = (
+          <>
+            <button onClick={() => {
+                // changePage(1);
+              navigate(`/page/1`);
+
+            }}>1</button>
+            <button>...</button>
+            <button onClick={() => {
+                // changePage(startValue);
+              navigate(`/page/${startValue}`);
+
+
+            }}>{startValue}</button>
+            {[...Array(5)].map((_, idx) => (
+              <button
+                key={startValue + idx + 1}
+                disabled={parseInt(page) == startValue + idx + 1}
+                onClick={() => {
+                    // changePage(startValue + idx + 1);
+                    navigate(`/page/${startValue + idx + 1}`);
+
+                }}
+              >
+                {startValue + idx + 1}
+              </button>
+            ))}
+            <button>...</button>
+            {/* last page */}
+            <button onClick={() => {
+                // changePage(pages);
+                navigate(`/page/${pages}`);
+
+            }}>{pages}</button>
+          </>
+        );
+      } else {
+        let amountLeft = pages - parseInt(page) + 5;
+        middlePagination = (
+          <>
+            <button onClick={() => {
+                // changePage(1);
+                navigate(`/page/1`);
+
+            }}>1</button>
+            <button>...</button>
+            <button onClick={() => {
+                // changePage(startValue);
+                navigate(`/page/${startValue}`);
+
+            }}>{startValue}</button>
+            {[...Array(amountLeft)].map((_, idx) => (
+              <button
+                key={startValue + idx + 1}
+                style={
+                  pages < startValue + idx + 1 ? { display: "none" } : null
+                }
+                disabled={parseInt(page) === startValue + idx + 1}
+                onClick={() => {
+                    // changePage(startValue + idx + 1);
+                    navigate(`/page/${startValue + idx + 1}`);
+
+                }}
+              >
+                {startValue + idx + 1}
+              </button>
+            ))}
+          </>
+        );
+      }
+    }
   }
   return (
     pages > 1 && (
